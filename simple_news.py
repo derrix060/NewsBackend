01421e98f67f9e5789ce61c50a9da2afb8b17630
derrix060 <mario_surfing@hotmail.com>
2017-05-27 17:20:05 -0300

    add some files

--- simple_news.py
+++ simple_news.py
@@ -1,31 +1,8 @@
 import newspaper
 
-cnn_paper = newspaper.build('https://www.uol.com.br/', language='pt', memoize_articles=False)
 
-"""
-for category in cnn_paper.category_urls():
-    print(category)
-"""
-"""
-for article in cnn_paper.articles:
-    print(article.url)
-"""
+cnn_paper = newspaper.build('http://computerworld.com.br/', language='pt',
+                             memoize_articles=False, fetch_images=False)
 
-article = newspaper.Article(url='http://revistatrip.uol.com.br/tpm/a-viuva-do-chorao-graziela-goncalves-em-um-depoimento-sobre-seus-dias-de-luto')
-article.download()
-article.parse()
-print(article.text)
-"""
-article = newspaper.Article(url='http://g1.globo.com/politica/operacao-lava-jato/noticia/pf-comeca-pericia-nos-gravadores-entregues-pela-defesa-de-joesley-batista.ghtml')
-article.download()
-article.parse()
-print(article.text)
-"""
-
-
-"""
-PT -> UOL
-EN -> CNN http://cnn.com
-FR -> http://www.lemonde.fr
-
-"""
+for art in cnn_paper.articles:
+    print(art.url)
